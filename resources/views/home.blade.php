e4661e48a2aa37952ab112e1417531707e23ac38
Jim Paulo Ovejera <jimpaulo.ovejera@achievewithoutborders.com>
2016-09-15 11:01:24 +0800

    Install Normalize.css and jQuery

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,83 +1,38 @@
-<!DOCTYPE html>
-<html lang="en">
+<!doctype html>
+<html class="no-js" lang="">
     <head>
         <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title></title>
+        <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>LogIn - Coming Soon!</title>
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <!-- Place favicon.ico in the root directory -->
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500" rel="stylesheet">
-        <link rel="stylesheet" href="/css/foundation.css" media="screen">
-        <link rel="stylesheet" href="/css/main.css" media="screen">
-
-        <!-- Styles -->
-        <style>
-          body {
-            font-family: 'Roboto', sans-serif;
-          }
-
-          .container {
-            position: absolute;
-            top: 0;
-            bottom: 0;
-            height: 700px;
-            left: 0;
-            right: 0;
-            text-align: center;
-            width: 40%;
-            margin: auto;
-          }
-
-          .container img {
-            width: 100%;
-          }
-
-          h1 {
-            font-size: 60px;
-            font-weight: 100;
-            text-shadow: 0px 0px 10px #652c90;
-            animation-name: text-anim;
-            animation-iteration-count: infinite;
-            animation-duration: 1s;
-            animation-direction: alternate;
-
-          }
-
-          @keyframes text-anim {
-            from {
-              text-shadow: 0px 0px 15px #652c90;
-            }
-            to {
-              text-shadow: 0px 0px 0px #652c90;
-            }
-          }
-
-          /* Portrait */
-          @media only screen
-            and (min-device-width: 320px)
-            and (max-device-width: 480px)
-            and (-webkit-min-device-pixel-ratio: 2)
-            and (orientation: portrait) {
-              h1 {
-                font-size: 25px;
-                font-weight: 100;
-              }
-
-              .container {
-                height: 180px;
-              }
-          }
-        </style>
+        <link rel="stylesheet" href="css/foundation.css">
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
     </head>
     <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
 
-      <div class="container">
-        <img src="img/logo.png" alt="LogIn logo" />
-        <h1>Coming Soon!</h1>
-      </div>
+        <!-- Add your site or application content here -->
+        <p>Hello world! This is HTML5 Boilerplate.</p>
 
+        <script src="js/jquery-3.1.0.min.js"></script>
+        <script src="js/app.js"></script>
 
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='https://www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+        </script>
     </body>
 </html>
