bede4a274adba572766ad9a6d5bb95b9ec2e4c04
Jim Paulo Ovejera <jimpaulo.ovejera@achievewithoutborders.com>
2016-09-16 10:17:35 +0800

    Create global responsive main menu

---
+++ resources/views/menu-bar.blade.php
@@ -0,0 +1,23 @@
+<div class="menu-container">
+  <nav class="row menu">
+    <div class="columns small-12 medium-5 large-7 logo">
+      <a href="/"><img src="img/logo.png" alt="logo" /></a>
+    </div>
+    <div class="columns small-4 medium-3 large-2 align-middle m-util get-support">
+      <div>
+          <span>Get Support</span>
+          <ul class="support-sub-menu">
+            <li><a href="/live-chat">Live Chat <img src="img/live-chat-2.png" alt="live chat" /></a></li>
+            <li><a href="/email">admin@loginsvc.com <img src="img/email.png" alt="email" /></a></li>
+            <li><a href="/phone">954.235.0693 <img src="img/phone-2.png" alt="phone" /></a></li>
+          </ul>
+      </div>
+    </div>
+    <div class="columns small-4 medium-3 large-2 align-middle m-util my-account">
+      <a href="#"><span>My Account</span></a>
+    </div>
+    <div class="columns small-4 medium-1 large-1 align-middle m-util my-cart">
+      <a href="#"><img src="img/cart.png" alt="cart" /><span class="cart-notif">3</span></a>
+    </div>
+  </nav>
+</div>
