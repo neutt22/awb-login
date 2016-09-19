4167e339f832151e2e85deef376a081dceab9345
Jim Paulo Ovejera <jimpaulo.ovejera@achievewithoutborders.com>
2016-09-19 09:29:18 +0800

    Add search animation

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,11 @@
 $(document).ready(function () {
 
-  
+  $('.search-container').on('click', function(){
+    $(this).css('box-shadow', '0 0 15px white');
+  });
+
+  $('.search-container').on('focusout', function(){
+    $(this).css('box-shadow', 'none');
+  });
 
 });
