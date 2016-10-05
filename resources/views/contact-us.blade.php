1e87455bee95fdab2309307ea35919eaed1f89de
Jim Paulo Ovejera <jimpaulo.ovejera@achievewithoutborders.com>
2016-10-06 06:13:54 +0800

    Add Google Map key, old method not working for production

--- resources/views/contact-us.blade.php
+++ resources/views/contact-us.blade.php
@@ -76,15 +76,12 @@
       </div>
       <div class="row">
         <div class="columns small-12">
-          <script src="https://maps.google.com/maps/api/js?sensor=false"></script>
-          <div style="overflow:hidden;height:400px;width:100%;position:relative">
-            <div id="gmap_canvas" style="height:400px;width:100%;">
-              <style>#gmap_canvas img{max-width:none!important;background:none!important}</style>
-            </div>
-          </div>
-          <script>
-            function init_map(){var myOptions = {scrollwheel: false, zoom:14,center:new google.maps.LatLng(14.5470363,121.0105857),mapTypeId: google.maps.MapTypeId.ROADMAP};map = new google.maps.Map(document.getElementById("gmap_canvas"), myOptions);marker = new google.maps.Marker({map: map,position: new google.maps.LatLng(14.5470363,121.0105857)});infowindow = new google.maps.InfoWindow({content:"<b>Achieve Without Borders, Inc.</b><br/>2029 Gen Mascardo St.,<br/> Makati City" });google.maps.event.addListener(marker, "click", function(){infowindow.open(map,marker);});infowindow.open(map,marker);}google.maps.event.addDomListener(window, 'load', init_map);
-          </script>
+          <iframe
+            width="100%"
+            height="450"
+            frameborder="0" style="border:0"
+            src="https://www.google.com/maps/embed/v1/place?key=AIzaSyD-nwfdRODbIw8GhghZxcVeW8-q8LAa7do&q=Achieve+Without+Borders,+Inc." allowfullscreen>
+          </iframe>
         </div>
       </div>
     </div>
