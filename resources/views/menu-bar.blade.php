f4f2badbc156479be9698805e33df0842d5e7bd8
Jim Paulo Ovejera <jimpaulo.ovejera@achievewithoutborders.com>
2016-10-06 09:32:44 +0800

    Revise global menu, add more elements

--- resources/views/menu-bar.blade.php
+++ resources/views/menu-bar.blade.php
@@ -1,8 +1,55 @@
 <div class="menu-container">
+  <div class="top-menu">
+    <div class="row">
+      <div class="columns small-12">
+        <ul>
+          <li><a href="/explore">EXPLORE</a></li>
+          <li><a href="/about-us">ABOUT US</a></li>
+          <li><a href="/contact-us">CONTACT US</a></li>
+          <li><a href="#">ENG / USD</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <nav>
+    <div class="row">
+      <div class="columns small-12">
+        <a href="/"><img class="logo" src="img/logo.png" alt="logo" /></a>
+        <ul>
+          <li>
+            <a href="/">HOME</a>
+          </li>
+          <li>
+            GET SUPPORT
+            <div class="sub-menu">
+              <ul>
+                <li>Live chat</li>
+                <li>954.235.0693</li>
+                <li>info@loginsvc.com</li>
+              </ul>
+            </div>
+          </li>
+          <li>
+            LOGIN / REGISTER
+            <div class="sub-menu">
+              <ul>
+                <li><a href="#">Dashboard</a></li>
+                <li><a href="#">History</a></li>
+                <li><a href="#">Settings</a></li>
+                <li><a href="#">Logout</a></li>
+              </ul>
+            </div>
+          </li>
+          <li><a href="/cart"><img class="cart" src="img/cart.png" alt="cart" /><span>3</span></a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  {{--
   <nav class="row menu">
     <div class="columns small-12 medium-5 large-7 logo">
       <a href="/"><img src="img/logo.png" alt="logo" /></a>
-      <!-- <input type="text" name="q" placeholder="Enter part number..."> -->
     </div>
     <div class="columns small-4 medium-3 large-2 align-middle m-util get-support">
       <div>
@@ -22,4 +69,5 @@
       <a href="/cart"><img src="img/cart.png" alt="cart" /><span class="cart-notif">3</span></a>
     </div>
   </nav>
+  --}}
 </div>
