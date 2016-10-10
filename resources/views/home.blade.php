a2d1cae131de273852c6dd78384b28ac863f9f97
Jim Paulo Ovejera <jimpaulo.ovejera@achievewithoutborders.com>
2016-10-10 08:42:28 +0800

    Add Explore section and Upcoming events section for home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -56,6 +56,116 @@
 
 @include('home-products')
 
+<div class="explore-container">
+  <div class="row">
+    <div class="columns small-12">
+      <header>
+        <h1>EXPLORE</h1>
+        <img src="img/plane.png" alt="plane" />
+      </header>
+    </div>
+  </div>
+  <div class="membership">
+    <div class="row">
+      <div class="columns large-6">
+        <h3>MEMBERSHIP BENEFITS</h3>
+        <ul>
+          <li>List your inventory to ship within 48 hours</li>
+          <li>Sell all your inventory easily without increasing your personnel</li>
+          <li>Ability to list your desire inventory and increase demand</li>
+          <li>24/7 Customer support by phone, email and chat</li>
+          <li>Create your own credit card services per transaction</li>
+          <li>Complementary 3 users per account</li>
+        </ul>
+        <div style="text-align: center;">
+
+          <div class="price">
+            <div></div>
+            <div>
+              <span>$327</span><span>.00</span>
+            </div>
+            <h4>MONTHLY FEE</h4>
+          </div>
+          <div class="price">
+            <div></div>
+            <div>
+              <span>$1,000</span><span>.00</span>
+            </div>
+            <h5>Initiation fee applies only to entities selling aircraft parts to the open market for first time.</h5>
+          </div>
+        </div>
+      </div>
+      <div class="columns large-6 align-self-middle">
+        <p>
+          Members glide on existing account for support<br> on all sale processes.
+        </p>
+        <div class="sign-up">
+          <a href="/register">SIGN ME UP</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="events">
+  <div class="row">
+    <div class="columns large-4">
+      <h1>Upcoming Events</h1>
+      <ul>
+        <li>
+          <a target="_blank" href="http://www.sun-n-fun.org/sun-n-fun-intl-fly-in-expo/">
+            <div class="row">
+              <div class="columns small-3">
+                <div class="event-date">
+                  <span>4 - 9</span><br>
+                  <span>OCT '17</span>
+                </div>
+              </div>
+              <div class="columns small-9">
+                <h3>SUN 'n FUN Int'l</h3>
+                <span>LAKELAND UNDER REGIONAL AIRPORT</span>
+              </div>
+            </div>
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="https://www.nbaa.org/events/date/">
+            <div class="row">
+              <div class="columns small-3">
+                <div class="event-date">
+                  <span>14 - 16</span><br>
+                  <span>FEB '17</span>
+                </div>
+              </div>
+              <div class="columns small-9">
+                <h3>NBAA 2017</h3>
+                <span>MIAMI, FL</span>
+              </div>
+            </div>
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="https://www.eaa.org/en/airventure">
+            <div class="row">
+              <div class="columns small-3">
+                <div class="event-date">
+                  <span>14 - 16</span><br>
+                  <span>FEB '17</span>
+                </div>
+              </div>
+              <div class="columns small-9">
+                <h3>OSHKOSH 2017</h3>
+                <span>OSHKOSH, WI</span>
+              </div>
+            </div>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</div>
+
+
 @endsection
 
 @section('custom-js')
